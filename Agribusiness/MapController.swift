62423913cee9956d52e91ad2a42f944ec683c245
Tiago Quaini <tiago.quaini@sap.com>
2018-05-24 11:20:15 -0300

    Draw a route in maps

--- Agribusiness/MapController.swift
+++ Agribusiness/MapController.swift
@@ -11,14 +11,15 @@ import GoogleMaps
 
 class MapController: UIViewController {
 
-    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet fileprivate weak var mapView: GMSMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 10.0)
         mapView.camera = camera
         mapView.settings.myLocationButton = true
         showMarker(position: camera.target)
+        fetchRoute()
     }
 
     func showMarker(position: CLLocationCoordinate2D){
@@ -29,8 +30,64 @@ class MapController: UIViewController {
         marker.map = mapView
     }
 
+    func fetchRoute() {
+        let origin = "Mountain View"
+        let destination = "San Jose"
+        let apiKey = "AIzaSyBVjW1BbO04oUZBRgNqp-hLr314w5LdA-U"
+        let urlString = "https://maps.googleapis.com/maps/api/directions/json"
+
+        let query: [String: String] = [
+            "key": apiKey,
+            "origin": origin,
+            "destination": destination
+        ]
+
+        let baseURL = URL(string: urlString)!
+        let url = baseURL.withQueries(query)!
+
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let data = data {
+                print("oss")
+                print(data)
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options:.allowFragments) as! [String : AnyObject]
+                    let routes = json["routes"] as! NSArray
+
+                    DispatchQueue.main.async {
+                        self.mapView.clear()
+
+                        for route in routes {
+                            let routeOverviewPolyline:NSDictionary = (route as! NSDictionary).value(forKey: "overview_polyline") as! NSDictionary
+                            let points = routeOverviewPolyline.object(forKey: "points")
+                            let path = GMSPath.init(fromEncodedPath: points! as! String)
+                            let polyline = GMSPolyline.init(path: path)
+                            polyline.strokeWidth = 3
+
+                            let bounds = GMSCoordinateBounds(path: path!)
+                            self.mapView!.animate(with: GMSCameraUpdate.fit(bounds, withPadding: 30.0))
+
+                            polyline.map = self.mapView
+                        }
+                    }
+                } catch let error as Error{
+                    print("error:\(error)")
+                }
+            }
+        }
+        task.resume()
+    }
 }
 
 extension MapController: GMSMapViewDelegate {
 
+}
+
+extension URL {
+    func withQueries(_ queries: [String: String]) -> URL? {
+        var components = URLComponents(url: self,
+                                       resolvingAgainstBaseURL: true)
+        components?.queryItems = queries.flatMap
+            { URLQueryItem(name: $0.0, value: $0.1) }
+        return components?.url
+    }
 }
