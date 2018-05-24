bc963e29ce3b79864408860476a654032a391abf
Tiago Quaini <tiago.quaini@sap.com>
2018-05-24 09:41:56 -0300

    Changing maps structure

--- Agribusiness/ViewController.swift
+++ Agribusiness/ViewController.swift
@@ -11,30 +11,26 @@ import GoogleMaps
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let camera = GMSCameraPosition.camera(withLatitude: 37.36, longitude: -122.0, zoom: 6.0)
+        mapView.camera = camera
+        mapView.settings.myLocationButton = true
+        showMarker(position: camera.target)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    override func loadView() {
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-
-        // Creates a marker in the center of the map.
+    func showMarker(position: CLLocationCoordinate2D){
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = position
+        marker.title = "Palo Alto"
+        marker.snippet = "San Francisco"
         marker.map = mapView
     }
 
 }
 
+extension ViewController: GMSMapViewDelegate {
+
+}
